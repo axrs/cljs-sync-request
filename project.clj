7491aebc30845076fd74c62cb3b4eee8dc29c7db
Alexander Scott <xander@cadcoder.com>
2019-05-29 22:38:16 +1000

    deploy-repos added

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
                  [org.clojure/clojurescript "1.10.520" :scope "provided"]]
   :clean-targets ["target"]
   :source-paths ["src"]
-  :test-paths ["test"])
+  :test-paths ["test"]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]])
