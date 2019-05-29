fc0e480e12b371b53d882cbe6d3d429879135d76
Alexander Scott <xander@cadcoder.com>
2019-05-29 21:28:01 +1000

    Update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject io.axrs/cljs-sync-request "0.0.1-SNAPSHOT"
-  :description "A ClojureScript wrapper around sync-request. Make synchronous web requests with cross-platform support."
+  :description "A ClojureScript wrapper around sync-request. Make synchronous HTTP requests."
   :min-lein-version "2.8.1"
   :dependencies [[thheller/shadow-cljs "2.8.37"]
                  [org.clojure/clojurescript "1.10.520"]]
